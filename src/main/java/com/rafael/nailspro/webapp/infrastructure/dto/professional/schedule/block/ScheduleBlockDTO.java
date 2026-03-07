5a55ea1f474623e92dee65258d9f03b7f36975de
rafadev <eooraff@gmail.com>
2026-03-07 13:57:08 -0300

    validation(schedule block)

--- src/main/java/com/rafael/nailspro/webapp/infrastructure/dto/professional/schedule/block/ScheduleBlockDTO.java
+++ src/main/java/com/rafael/nailspro/webapp/infrastructure/dto/professional/schedule/block/ScheduleBlockDTO.java
@@ -1,14 +1,21 @@
 package com.rafael.nailspro.webapp.infrastructure.dto.professional.schedule.block;
 
-import lombok.Builder;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.ZonedDateTime;
 
-@Builder
 public record ScheduleBlockDTO(
+
         Long id,
+
+        @NotNull(message = "A data e hora de início do bloqueio são obrigatórias.")
         ZonedDateTime dateAndStartTime,
+
+        @NotNull(message = "A data e hora de término do bloqueio são obrigatórias.")
         ZonedDateTime dateAndEndTime,
+
+        @NotNull(message = "É necessário informar se o bloqueio é para o dia inteiro.")
         Boolean isWholeDayBlocked,
+
         String reason
 ) {}

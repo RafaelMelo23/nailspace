616508d037a3b91bda29927f5dcf0db95b038d38
rafadev <eooraff@gmail.com>
2026-03-11 11:17:15 -0300

    feat(validation): adds webhook validation for evo api

--- src/main/java/com/rafael/nailspro/webapp/infrastructure/config/WebConfig.java
+++ src/main/java/com/rafael/nailspro/webapp/infrastructure/config/WebConfig.java
@@ -1,9 +1,11 @@
 package com.rafael.nailspro.webapp.infrastructure.config;
 
+import com.rafael.nailspro.webapp.infrastructure.security.interceptor.EvolutionApiInterceptor;
 import com.rafael.nailspro.webapp.infrastructure.security.interceptor.SalonMaintenanceInterceptor;
 import com.rafael.nailspro.webapp.infrastructure.security.interceptor.TenantStatusInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,11 +14,13 @@ public class WebConfig implements WebMvcConfigurer {
 
     private final SalonMaintenanceInterceptor salonMaintenanceInterceptor;
     private final TenantStatusInterceptor tenantStatusInterceptor;
+    private final EvolutionApiInterceptor evolutionApiInterceptor;
 
     //todo: currently disabled for dev
-    //@Override
-    //public void addInterceptors(InterceptorRegistry registry) {
-    //    registry.addInterceptor(salonMaintenanceInterceptor);
-    //    registry.addInterceptor(tenantStatusFilter);
-    //}
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //    registry.addInterceptor(salonMaintenanceInterceptor);
+        //    registry.addInterceptor(tenantStatusFilter);
+        registry.addInterceptor(evolutionApiInterceptor);
+    }
 }
---
+++ src/main/java/com/rafael/nailspro/webapp/infrastructure/security/interceptor/EvolutionApiInterceptor.java
@@ -0,0 +1,32 @@
+package com.rafael.nailspro.webapp.infrastructure.security.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+@RequiredArgsConstructor
+public class EvolutionApiInterceptor implements HandlerInterceptor {
+
+    private final String EVO_WEBHOOK_ENDPOINT = "/api/v1/webhook";
+    @Value("${evolution.apikey}")
+    private String evolutionApiKey;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        String path = request.getRequestURI();
+
+        if (EVO_WEBHOOK_ENDPOINT.equalsIgnoreCase(path)) {
+            String apiKey = request.getHeader("apiKey");
+
+            if (!evolutionApiKey.equalsIgnoreCase(apiKey)) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return true;
+            }
+        }
+        return false;
+    }
+}
